00a3e554415bfab7517faa7157c9fe07e6f8f432
Olivier Hoareau <oha@greenberets.io>
2020-07-17 14:59:53 +0200

    templates-core: fix

--- packages/templates-core/__fixtures__/makefiles/sample-front-with-publish-image.mk
+++ packages/templates-core/__fixtures__/makefiles/sample-front-with-publish-image.mk
@@ -17,7 +17,9 @@ deploy: deploy-publish-image
 deploy-code:
 	@AWS_PROFILE=$(AWS_PROFILE) aws s3 sync public/ s3://$(bucket) --delete
 deploy-publish-image:
-	@(aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 012345678912.dkr.ecr.eu-west-3.amazonaws.com) && (docker push 012345678912.dkr.ecr.eu-west-3.amazonaws.com/abcd:latest)
+	@AWS_PROFILE=$(AWS_PROFILE) aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 012345678912.dkr.ecr.eu-west-3.amazonaws.com
+	@docker push 012345678912.dkr.ecr.eu-west-3.amazonaws.com/abcd:latest
+	@docker logout 012345678912.dkr.ecr.eu-west-3.amazonaws.com 2>/dev/null || true
 deploy-raw: deploy-code invalidate-cache
 
 generate-env-local:
--- packages/templates-core/src/targets/AbstractDockerTarget.ts
+++ packages/templates-core/src/targets/AbstractDockerTarget.ts
@@ -13,21 +13,34 @@ export abstract class AbstractDockerTarget extends GenericTarget {
         return <string[]>[];
     }
     getPreCommands(options: any): string[] {
-        const pres = <string[]>[];
-        if (options.awsEcrLogin) {
-            let {region, domain} = parseEcr(options.tag);
-            options.region && (region = options.region);
-            options.domain && (domain = options.domain);
-            pres.push(`(aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${domain})`);
-        }
-        return pres;
+        return <string[]>[];
     }
 
     getPostCommands(options: any): string[] {
         return <string[]>[];
     }
+    buildPreCli(options: any): string[] {
+        const pres = <string[]>[];
+        if (options.awsEcrLogin) {
+            let {region, domain} = parseEcr(options.tag);
+            options.region && (region = options.region);
+            options.domain && (domain = options.domain);
+            pres.push(`aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${domain}`);
+        }
+        return pres;
+    }
+    buildPostCli(options: any): string[] {
+        const posts = <string[]>[];
+        if (options.awsEcrLogout) {
+            let {domain} = parseEcr(options.tag);
+            options.domain && (domain = options.domain);
+            posts.push(`docker logout ${domain} 2>/dev/null || true`);
+        }
+        return posts;
+    }
     buildSteps(options: any) {
         return [
+            ...this.buildPreCli(options),
             this.buildCli(
                 `docker ${this.getCommandName(options)}`,
                 this.getCommandArgs(options),
@@ -36,7 +49,8 @@ export abstract class AbstractDockerTarget extends GenericTarget {
                 this.getCommandPreOptions(options),
                 this.getPreCommands(options),
                 this.getPostCommands(options),
-            )
+            ),
+            ...this.buildPostCli(options)
         ];
     }
 }
